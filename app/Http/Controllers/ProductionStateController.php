5827ea0906e3a9d594da46b88ed37c6b00754d75
michelbras <michel.bras@gewoongers.nl>
2023-02-10 13:35:25 +0100

    return statement added

--- app/Http/Controllers/ProductionStateController.php
+++ app/Http/Controllers/ProductionStateController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class ProductionStateController extends Controller
 {
-    public function index()
+    public function index(): string
     {
-        $productionStates = file_get_contents(storage_path() . "/data/ProductieStaat.json");
-
-        print_r($productionStates);
+        return file_get_contents(storage_path() . "/data/ProductieStaat.json");
     }
 }
